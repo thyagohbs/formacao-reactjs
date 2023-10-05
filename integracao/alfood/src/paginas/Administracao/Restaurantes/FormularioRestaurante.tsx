eeb06ec7bbf0bc1fdc1750a52bff4a6966c69b38
Thyago Henrique <thyagobs.al@gmail.com>
2023-10-05 10:27:10 -0300

    update

--- integracao/alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ integracao/alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,20 +1,45 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 const FormularioRestaurante = () => {
   const [nomeRestaurante, setNomeRestaurante] = useState("");
 
+  const parametros = useParams()
+
+  useEffect(() => {
+    if(parametros.id){
+      axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+      .then(resposta => setNomeRestaurante(resposta.data.nome))
+    }
+  }, [parametros])
+  
+
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
 
-    axios.post("http://localhost:8000/api/v2/restaurantes/", {
+    if(parametros.id){
+
+      axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
         nome: nomeRestaurante
       })
 
       .then(() => {
-        alert("Restaurante cadastrado com sucesso!");
+        alert("Restaurante atualizado com sucesso!");
       });
+
+    }else{
+
+      axios.post("http://localhost:8000/api/v2/restaurantes/", {
+          nome: nomeRestaurante
+        })
+
+        .then(() => {
+          alert("Restaurante cadastrado com sucesso!");
+        });
+    }
   };
 
   return (
@@ -24,8 +49,10 @@ const FormularioRestaurante = () => {
         onChange={(evento) => setNomeRestaurante(evento.target.value)}
         label="Nome do Restaurante"
         variant="standard"
+        fullWidth
+        required
       />
-      <Button type="submit" variant="outlined">
+      <Button sx={{ marginTop: 1}} type="submit" fullWidth variant="outlined">
         Salvar
       </Button>
     </form>
