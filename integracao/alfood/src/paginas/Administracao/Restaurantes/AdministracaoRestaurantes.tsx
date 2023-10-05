2a02915f6426031bbf7834559738c0638bda5be3
Thyago Henrique <thyagobs.al@gmail.com>
2023-10-05 11:27:53 -0300

    update

--- integracao/alfood/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ integracao/alfood/src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Paper,
   Table,
   TableBody,
@@ -10,6 +11,7 @@ import {
 import axios from "axios";
 import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
+import { Link } from "react-router-dom";
 
 const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
@@ -20,18 +22,47 @@ const AdministracaoRestaurantes = () => {
       .then((resposta) => setRestaurantes(resposta.data));
   }, []);
 
+  const excluir = (restauranteAhSerExcluido: IRestaurante) => {
+    axios
+      .delete(
+        `http://localhost:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`
+      )
+      .then(() => {
+        const listaRestaurante = restaurantes.filter(
+          (restaurante) => restaurante.id !== restauranteAhSerExcluido.id
+        );
+        setRestaurantes([...listaRestaurante])
+
+      });
+  };
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
+            <TableCell>Excluir</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map((restaurante) => (
             <TableRow key={restaurante.id}>
               <TableCell>{restaurante.nome}</TableCell>
+              <TableCell>
+                [{" "}
+                <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>{" "}
+                ]
+              </TableCell>
+              <TableCell>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => excluir(restaurante)}
+                >
+                  Excluir
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
