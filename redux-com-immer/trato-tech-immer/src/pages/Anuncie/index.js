a04f55609cf53b29fdceccc0b4eb9f425a733570
Thyago Henrique <thyagobs.al@gmail.com>
2023-10-23 05:37:00 -0300

    update

--- redux-com-immer/trato-tech-immer/src/pages/Anuncie/index.js
+++ redux-com-immer/trato-tech-immer/src/pages/Anuncie/index.js
@@ -1,23 +1,22 @@
 import Button from 'components/Button';
 import Header from 'components/Header';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Anuncie.module.scss';
 import { useForm } from 'react-hook-form';
+import { cadastrarItem } from 'store/reducers/itens';
 
 export default function Anuncie() {
+  const dispatch = useDispatch();
   const categorias = useSelector(state => state.categorias.map(({ nome, id }) => ({ nome, id })));
-  const { register, handleSubmit, formState } = useForm({
+  const { register, handleSubmit } = useForm({
     defaultValues: {
       categoria: ''
     }
   });
-  const { errors } = formState;
 
-  function cadastrar(parametro) {
-    console.log('parametro: ', parametro);
+  function cadastrar(data) {
+    dispatch(cadastrarItem(data));
   }
-
-  console.log(errors) // Para ver o objeto errors mudando!
 
   return (
     <div className={styles.container}>
@@ -26,13 +25,10 @@ export default function Anuncie() {
         descricao='Anuncie seu produto no melhor site do Brasil!'
       />
       <form className={styles.formulario} onSubmit={handleSubmit(cadastrar)}>
-        <input className={errors.nome ? styles['input-erro'] : ''} {...register('nome', { required: 'O campo nome é obrigatório' })} placeholder='Nome do produto' alt='nome do produto' />
-        {errors.nome && <span className={styles['mensagem-erro']}> {errors.nome.message} </span>}
-        <input className={errors.descricao ? styles['input-erro'] : ''}  {...register('descricao', { required: 'O campo descricao é obrigatório' })} placeholder='Descrição do produto' alt='descrição do produto' />
-        {errors.descricao && <span className={styles['mensagem-erro']}> {errors.descricao.message} </span>}
-        <input className={errors.imagem ? styles['input-erro'] : ''}  {...register('imagem', { required: 'O campo imagem é obrigatório' })} placeholder='URL da imagem do produto' alt='URL da imagem do produto' />
-        {errors.imagem && <span className={styles['mensagem-erro']}> {errors.imagem.message} </span>}
-        <select className={errors.categoria ? styles['input-erro'] : ''}  {...register('categoria', { required: 'O campo categoria é obrigatório' })}>
+        <input {...register('nome', { required: true })} placeholder='Nome do produto' alt='nome do produto' />
+        <input {...register('descricao', { required: true })} placeholder='Descrição do produto' alt='descrição do produto' />
+        <input {...register('imagem', { required: true })} placeholder='URL da imagem do produto' alt='URL da imagem do produto' />
+        <select {...register('categoria', { required: true })}>
           <option value='' disabled > Selecione a categoria </option>
           {categorias.map(categoria => (
             <option key={categoria.id} value={categoria.id}>
@@ -40,9 +36,7 @@ export default function Anuncie() {
             </option>
           ))}
         </select>
-        {errors.categoria && <span className={styles['mensagem-erro']}> {errors.categoria.message} </span>}
-        <input className={errors.preco ? styles['input-erro'] : ''}  {...register('preco', { required: 'O campo preco é obrigatório' })} type='number' placeholder='Preço do produto' />
-        {errors.preco && <span className={styles['mensagem-erro']}> {errors.preco.message} </span>}
+        <input {...register('preco', { required: true })} type='number' placeholder='Preço do produto' />
         <Button type='submit'>
           Cadastrar produto
         </Button>
